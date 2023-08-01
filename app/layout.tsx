75a899db0222b60ebb81395f1ab1f85070bcf8de
zinuu4 <unvm7721@gmail.com>
2023-08-02 00:00:52 +0300

    Footer implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
 
 import "./globals.scss";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
