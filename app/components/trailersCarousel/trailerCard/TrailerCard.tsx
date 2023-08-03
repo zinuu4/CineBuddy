a2dbbb58dbb1eb160a67b53a6ef53cc55f46f775
almalmalm <almostkalinin@gmail.com>
2023-08-03 20:07:40 +0400

    feat: add static data to slider cards

--- app/components/trailersCarousel/trailerCard/TrailerCard.tsx
+++ app/components/trailersCarousel/trailerCard/TrailerCard.tsx
@@ -3,20 +3,34 @@ import styles from './trailerCard.module.scss';
 import Image from 'next/image';
 
 interface ITrailerCardProps {
-  slideUrl: string;
+  props: {
+    url: string;
+    title: string;
+    rating: number;
+    year: number;
+    genre: string;
+  };
 }
 
-export const TrailerCard: React.FC<ITrailerCardProps> = ({ slideUrl }) => {
+export const TrailerCard: React.FC<ITrailerCardProps> = ({ props }) => {
   return (
     <div className={styles.card}>
       <Image
-        src={slideUrl}
+        src={props.url}
         alt="Image"
         fill
         className={styles.img}
         sizes="width: 100%,   height: 30vw"
         priority={true}
       />
+      <div className={styles.content}>
+        <h2 className={styles.title}>{props.title}</h2>
+        <div className={styles.properties}>
+          <span className={styles.rating}>{props.rating.toFixed(1)}</span>
+          <span className={styles.year}>{props.year}</span>
+          <span className={styles.genre}>{props.genre}</span>
+        </div>
+      </div>
     </div>
   );
 };
