281cb701736d1dca4e43ada1fcfbc5751ae16bdb
zinuu4 <unvm7721@gmail.com>
2023-08-06 14:21:26 +0300

    fix: fix conflicts

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,5 @@
 import { CategoriesCarousel } from "@/widgets/categories-carousel";
-import { CategoriesList } from "@/entities/category/ui/categories-list";
+import { CategoriesList } from "@/pages/home/ui/categories-list";
 import { TrailersCarousel } from "@/widgets/trailers-carousel";
 
 export default function Home() {
