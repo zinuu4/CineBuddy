9b99997f8dbaa63da2bab59c5448dcfdc0b2770a
zinuu4 <unvm7721@gmail.com>
2023-08-08 21:17:14 +0300

    feat: facts show more functionality implementation

--- src/pages/movie/ui/facts-list/ui.tsx
+++ src/pages/movie/ui/facts-list/ui.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FactItem } from '@/entities/fact/ui/fact-card';
 import { IFactInMovie } from '@/shared/api';
@@ -13,16 +13,31 @@ interface IFactsListProps {
   facts: IFactInMovie[];
 }
 
-export const FactsList: React.FC<IFactsListProps> = ({ facts }) => (
-  <section>
-    <div className={classNames(styles.container, 'container')}>
-      <Title title="Знаете ли вы, что..." className={styles.title} />
-      <ul className="list-reset">
-        {facts.map(({ value }) => (
-          <FactItem key={value} text={cleanHtmlFromText(value) ?? ''} />
-        ))}
-      </ul>
-      <Button className={styles.btn}>Показать еще</Button>
-    </div>
-  </section>
-);
+export const FactsList: React.FC<IFactsListProps> = ({ facts }) => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const croppedFacts = facts.slice(0, 5);
+
+  const selectedFacts = isExpanded ? facts : croppedFacts;
+
+  return (
+    <section>
+      <div className={classNames(styles.container, 'container')}>
+        <Title title="Знаете ли вы, что..." className={styles.title} />
+        <ul className="list-reset">
+          {selectedFacts.map(({ value }) => (
+            <FactItem key={value} text={cleanHtmlFromText(value) ?? ''} />
+          ))}
+        </ul>
+        {facts.length >= 5 && (
+          <Button
+            onClick={() => setIsExpanded(!isExpanded)}
+            className={styles.btn}
+          >
+            {isExpanded ? 'Скрыть' : 'Показать еще'}
+          </Button>
+        )}
+      </div>
+    </section>
+  );
+};
