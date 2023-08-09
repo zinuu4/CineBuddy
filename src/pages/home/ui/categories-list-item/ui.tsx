39e0866ee903fa04ab32d627f1b600e7d1745234
zinuu4 <unvm7721@gmail.com>
2023-08-09 17:50:57 +0300

    refactor and feat: optimise title and add link and query params to categories titles

--- src/pages/home/ui/categories-list-item/ui.tsx
+++ src/pages/home/ui/categories-list-item/ui.tsx
@@ -1,25 +1,31 @@
 'use client';
 
 import classNames from 'classnames';
+import Link from 'next/link';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React, { useEffect } from 'react';
 
 import { MoviesSlider } from '@/widgets/movies-slider';
 import { ICategoriesApiProps, useGetMoviesQuery } from '@/entities/movie/api';
-
+import { createQueryString } from '@/shared/lib/helpers';
 import { ErrorMsg } from '@/shared/ui/error-msg';
 import { Loader } from '@/shared/ui/loader';
-import { TitleChevron } from '@/shared/ui/title-chevron';
+import { Title } from '@/shared/ui/title';
 
 import styles from './styles.module.scss';
 
 interface CategoriesListItemProps {
-  title: string;
   params: ICategoriesApiProps;
+  title: string;
+  queryName: string;
+  queryValue: string;
 }
 
 export const CategoriesListItem: React.FC<CategoriesListItemProps> = ({
   params,
   title,
+  queryName,
+  queryValue,
 }) => {
   const { data, isError } = useGetMoviesQuery(params);
 
@@ -29,10 +35,28 @@ export const CategoriesListItem: React.FC<CategoriesListItemProps> = ({
     ErrorMsg(isError);
   }, [isError]);
 
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const onChooseCategory = () => {
+    if (searchParams) {
+      router.push(
+        `${pathname}films?${createQueryString(
+          queryName,
+          queryValue,
+          searchParams,
+        )}`,
+      );
+    }
+  };
+
   return (
     <section>
       <div className={classNames(styles.container, 'container')}>
-        <TitleChevron title={title} />
+        <div onClick={onChooseCategory}>
+          <Title href="/films" chevron as={Link} title={title} />
+        </div>
         {data ? <MoviesSlider data={sliderData} /> : <Loader />}
       </div>
     </section>
