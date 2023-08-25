ce2eea2bb85284c1be24fa4ecdc7d22fc8f52b6f
zinuu4 <unvm7721@gmail.com>
2023-08-25 19:13:58 +0300

    update: update github tests and package.json scripts

--- src/shared/ui/label/ui.tsx
+++ src/shared/ui/label/ui.tsx
@@ -3,12 +3,8 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 
-interface LabelProps
-  extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {}
 
 export const Label: React.FC<LabelProps> = ({ className, ...rest }) => (
-  <label
-    className={classNames(styles.label, className)}
-    {...rest}
-  />
+  <label className={classNames(styles.label, className)} {...rest} />
 );
